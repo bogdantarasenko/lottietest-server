f2e3b49f674a8e53dbeee7618ba04509daa67a29
Bogdan Tarasenko <bogdantarasenko@Bogdans-MacBook-Pro.local>
2024-02-25 13:44:09 +0100

    rename APP_PORT to PORT to fix issue with api access

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -26,7 +26,7 @@ class EnvironmentVariablesValidator {
   @Min(0)
   @Max(65535)
   @IsOptional()
-  APP_PORT: number;
+  PORT: number;
 
   @IsUrl({ require_tld: false })
   @IsOptional()
@@ -46,8 +46,8 @@ export default registerAs<AppConfig>('app', () => {
     nodeEnv: process.env.NODE_ENV || 'development',
     workingDirectory: process.env.PWD || process.cwd(),
     frontendDomain: process.env.FRONTEND_DOMAIN,
-    port: process.env.APP_PORT
-      ? parseInt(process.env.APP_PORT, 10)
+    port: process.env.PORT
+      ? parseInt(process.env.PORT, 10)
       : process.env.PORT
         ? parseInt(process.env.PORT, 10)
         : 3000,
